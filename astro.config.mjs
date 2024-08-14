8e22e1c7b024c9df38e831f46eaa8ab69f736a60
Arghya Das <arghyadasproject@gmail.com>
2024-08-14 09:58:02 +0530

    update to the astro blog + framer motion

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,22 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
+import remarkToc from "remark-toc";
+import rehypePresetMinify from "rehype-preset-minify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [
+    tailwind(),
+    react(),
+    mdx({
+      syntaxHighlight: "shiki",
+      shikiConfig: { theme: "dracula" },
+      remarkPlugins: [remarkToc],
+      rehypePlugins: [rehypePresetMinify],
+      remarkRehype: { footnoteLabel: "Footnotes" },
+      gfm: false,
+    }),
+  ],
 });
--- src/pages/blog/_story.md
+++
@@ -1,9 +0,0 @@
----
-title: "My First Blog"
-date: "20247-04-30"
-author: "Arghya Das"
----
-
-## A Small Breakdown
-
-In this blog, I will be sharing a small breakdown of my first blog. I hope this will give you a better understanding of the skills and my CSS style. Stay tuned for more breakdowns in the future!
