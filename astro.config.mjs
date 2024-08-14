543d4d0f1517c075774648e4274a93c38ef52047
Arghya Das <arghyadasproject@gmail.com>
2024-08-14 10:09:45 +0530

    removed unnecessary imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,21 +2,8 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
-import remarkToc from "remark-toc";
-import rehypePresetMinify from "rehype-preset-minify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    react(),
-    mdx({
-      syntaxHighlight: "shiki",
-      shikiConfig: { theme: "dracula" },
-      remarkPlugins: [remarkToc],
-      rehypePlugins: [rehypePresetMinify],
-      remarkRehype: { footnoteLabel: "Footnotes" },
-      gfm: false,
-    }),
-  ],
+  integrations: [tailwind(), react(), mdx()],
 });
